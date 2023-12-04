ada7aded009ccffd67149e263545c9d1590aca09
Max <141071075+CakeDayZ@users.noreply.github.com>
2023-12-04 12:00:31 -0500

    Update day4.py

--- day4.py
+++ day4.py
@@ -14,14 +14,11 @@ def compareLists(win_list,check_list):
     #if matches: score = 2**(matches-1)
     #else: score = 0
     #print(score)
-    #return score 
-    '''
+    #return score '''
     return matches
 file = open("D:\Python\CodeAdvent2023\day4_input.txt","r")
 list = file.readlines()
 #score = 0
-#big_win_list = []
-#big_check_list = []
 match_list = []
 copy_list = []
 card_count =0
@@ -29,18 +26,14 @@ for i in list:
     win_nums_string = i.split(":")[1].split("|")[0].rstrip()
     win_count = int(len(win_nums_string))
     win_nums_list = stringToNumList(win_nums_string,win_count)
-    #big_win_list.append(win_nums_list)
     check_nums_string = i.split("|")[1].rstrip()
     check_count = int(len(check_nums_string))
     check_nums_list =stringToNumList(check_nums_string,check_count)
-    #big_check_list.append(check_nums_list)
     #score +=compareLists(win_nums_list,check_nums_list) #part 1
     match_list.append(compareLists(win_nums_list,check_nums_list))
     copy_list.append(1)
     card_count +=1
 #print(score) #part 1
-#print(big_win_list)
-#print(big_check_list)
 #print(match_list)
 #print(copy_list)
 for i in range(card_count): 
@@ -50,9 +43,7 @@ for i in range(card_count):
         copy_list[current] += copy_list[i]
         dupes-=1
         current +=1
-    print(copy_list)
-print(copy_list)
+    #print(copy_list)
 score2=0
 for i in copy_list: score2+=i
 print(score2)
-    
